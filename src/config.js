d2f401a8f8ed4f460e2bcaa2f8eb9cb5fd9f6245
Colton <coltoncejones12@gmail.com>
2019-10-01 15:26:28 -0400

    blah

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://psotgres@localhost/noteful',
+    DATABASE_URL: process.env.DATABASE_URL || 'https://git.heroku.com/afternoon-scrubland-66268.git',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/noteful-test'
 }
