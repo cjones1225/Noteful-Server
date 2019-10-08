301152518e524df181f04ec9d595d9007714810b
Colton <coltoncejones12@gmail.com>
2019-10-08 16:10:27 -0400

    fixing migrations

--- postgrator-config.js
+++ postgrator-config.js
@@ -8,5 +8,5 @@ module.exports = {
     "database": process.env.MIGRATION_DATABASE_NAME,
     "username": process.env.MIGRATION_DATABASE_USER,
     "password": process.env.MIGRATION_DATABASE_PASS,
-    "ssl": !!process.env.SSL
+    "ssl": !!process.env.SSL,
 }
