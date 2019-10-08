592bf85beed4095887751d2256c7b3bf3581cbc9
Colton <coltoncejones12@gmail.com>
2019-10-08 16:15:23 -0400

    fixing migrations

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,10 +3,13 @@ require('dotenv').config();
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
-    "host": process.env.MIGRATION_DATABASE_HOST,
-    "port": process.env.MIGRATION_DATABASE_PORT,
-    "database": process.env.MIGRATION_DATABASE_NAME,
-    "username": process.env.MIGRATION_DATABASE_USER,
-    "password": process.env.MIGRATION_DATABASE_PASS,
-    "ssl": !!process.env.SSL,
+    // "host": process.env.MIGRATION_DATABASE_HOST,
+    // "port": process.env.MIGRATION_DATABASE_PORT,
+    // "database": process.env.MIGRATION_DATABASE_NAME,
+    // "username": process.env.MIGRATION_DATABASE_USER,
+    // "password": process.env.MIGRATION_DATABASE_PASS,
+    // "ssl": !!process.env.SSL,
+    "connectionString": (process.env.NODE_ENV === 'test')
+        ? process.env.TEST_DB_URL
+        : process.env.DB_URL,
 }
